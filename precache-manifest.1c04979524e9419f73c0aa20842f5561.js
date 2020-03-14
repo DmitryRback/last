f0650db2690bdb7b9cf16948ff38377bbb31f7df
DmitryRback <dmitry.riback@gmail.com>
2020-03-14 12:58:51 +0200

    Updates

---
+++ precache-manifest.1c04979524e9419f73c0aa20842f5561.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7ae2d323ca21d9da5047f774a2d2869",
+    "url": "/myapp/index.html"
+  },
+  {
+    "revision": "d487e2344dee5b375943",
+    "url": "/myapp/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "445ba0b46b32c0c67fec",
+    "url": "/myapp/static/js/2.e86cc808.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/myapp/static/js/2.e86cc808.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d487e2344dee5b375943",
+    "url": "/myapp/static/js/main.f5fada8b.chunk.js"
+  },
+  {
+    "revision": "1716b5b1f163082744bb",
+    "url": "/myapp/static/js/runtime-main.aa379031.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/myapp/static/media/logo.5d5d9eef.svg"
+  }
+]);
